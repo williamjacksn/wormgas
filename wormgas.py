079b7ba3d53f718b36b79715c7f199370e54f08d
William Jackson <william@subtlecoolness.com>
2011-08-01 15:30:06 -0400

    Add !8ball and !config commands
    - Add !8ball and !config commands
    - Add docstrings where needed

--- wormgas.py
+++ wormgas.py
@@ -14,7 +14,7 @@ class wormgas(SingleServerIRCBot):
     def __init__(self):
         self.abspath = os.path.abspath(__file__)
         (self.path, self.file) = os.path.split(self.abspath)
-        self.cdbh = sqlite3.connect("%s/config.sqlite" % (self.path,),
+        self.cdbh = sqlite3.connect("%s/config.sqlite" % self.path,
                                     isolation_level=None)
         self.ccur = self.cdbh.cursor()
         server = self.get_config("irc:server")
@@ -25,23 +25,108 @@ class wormgas(SingleServerIRCBot):
     def __del__(self):
         self.cdbh.close()
 
-    def handle_flip(self):
+    def handle_8ball(self):
+        """Ask a question of the magic 8ball
+
+        Returns: a list of strings"""
 
         rs = []
-        rs.append(random.choice(("Heads!", "Tails!")))
+        answers = ("As I see it, yes.",
+                   "Ask again later.",
+                   "Better not tell you now.",
+                   "Cannot predict now.",
+                   "Concentrate and ask again.",
+                   "Don't count on it.",
+                   "It is certain.",
+                   "It is decidedly so.",
+                   "Most likely.",
+                   "My reply is no.",
+                   "My sources say no.",
+                   "Outlook good.",
+                   "Outlook not so good.",
+                   "Reply hazy, try again.",
+                   "Signs point to yes.",
+                   "Very doubtful.",
+                   "Without a doubt.",
+                   "Yes.",
+                   "Yes - definitely.",
+                   "You may rely on it.")
+        rs.append(random.choice(answers))
+        return(rs)
+
+    def handle_config(self, id=None, value=None):
+        """View or change config values
+
+        Arguments:
+            id: the config_id you want to view or change (leave empty to show
+                all available config_ids)
+            value: the value to change config_id to (leave empty to view current
+                value)
+
+        Returns: a list of strings"""
+
+        rs = []
+
+        if id and value:
+            self.set_config(id, value)
+            rs.append("%s = %s" % (id, value))
+        elif id:
+            rs.append("%s = %s" % (id, self.get_config(id)))
+        else:
+            cids = []
+            sql = "select distinct config_id from botconfig"
+            self.ccur.execute(sql)
+            for r in self.ccur:
+                cids.append(r[0])
+            mlcl = int(self.get_config("maxlength:configlist"))
+            while len(cids) > mlcl:
+                clist = cids[:mlcl]
+                cids[0:mlcl] = []
+                rs.append(", ".join(clist))
+            rs.append(", ".join(cids))
+
+        return(rs)
+
+    def handle_flip(self):
+        """Simulate a coin flip
+
+        Returns: a list of strings"""
+
+        rs = []
+        answers = ("Heads!", "Tails!")
+        rs.append(random.choice(answers))
         return(rs)
 
     def handle_help(self, priv=0, topic="all"):
+        """Look up help about a topic
+
+        Arguments:
+            priv: integer, the privilege level of the person asking for help
+            topic: string, the topic the person wants help about
+
+        Returns: a list of strings"""
 
         rs = []
 
         if topic == "all":
             rs.append("Use \x02!help [<topic>]\x0f with one of these topics: "
-                "flip")
+                "8ball, flip")
             if priv > 0:
                 rs.append("Level 1 administration topics: (none)")
             if priv > 1:
                 rs.append("Level 2 administration topics: stop")
+        elif topic == "8ball":
+            rs.append("Use \x02!8ball\x0f to ask a question of the magic 8ball")
+        elif topic == "config":
+            if priv > 1:
+                rs.append("Use \x02!config [<id>] [<value>]\x0f to display or "
+                    "change configuration settings")
+                rs.append("Leave off <value> to see the current setting, or "
+                    "use a <value> of -1 to remove a setting")
+                rs.append("Leave off <id> and <value> to see a list of all "
+                    "available config ids")
+            else:
+                rs.append("You are not permitted to use this command")
         elif topic == "flip":
             rs.append("Use \x02!flip\x0f to flip a coin")
         elif topic == "stop":
@@ -50,30 +135,54 @@ class wormgas(SingleServerIRCBot):
             else:
                 rs.append("You are not permitted to use this command")
         else:
-            rs.append("I can't help you with '%s'" % topic)
+            rs.append("I cannot help you with '%s'" % topic)
 
         return(rs)
 
-
     def on_privmsg(self, c, e):
-        # e.source() == nick!user@host
-        # e.arguments()[0] == text of message
+        """This method is called when a message is sent directly to the bot
+
+        Arguments:
+            c: the Connection object associated with this event
+            e: the Event object"""
 
         nick = e.source().split("!")[0]
         priv = self.get_config("privlevel:%s" % nick)
         msg = e.arguments()[0].strip()
         cmdtokens = msg.split()
+        try:
+            cmd = cmdtokens[0]
+        except IndexError:
+            cmd = None
 
         rs = []
 
+        # !8ball
+
+        if cmd == "!8ball":
+            rs = self.handle_8ball()
+
+        # !config
+
+        elif priv > 1 and cmd == "!config":
+            try:
+                id = cmdtokens[1]
+            except IndexError:
+                id = None
+            try:
+                value = cmdtokens[2]
+            except IndexError:
+                value = None
+            rs = self.handle_config(id, value)
+
         # !flip
 
-        if cmdtokens[0] == "!flip":
+        elif cmd == "!flip":
             rs = self.handle_flip()
 
         # !help
 
-        elif cmdtokens[0] == "!help":
+        elif cmd == "!help":
             try:
                 topic = cmdtokens[1]
             except IndexError:
@@ -82,7 +191,7 @@ class wormgas(SingleServerIRCBot):
 
         # !stop
 
-        elif (priv > 1) and (cmdtokens[0] == "!stop"):
+        elif priv > 1 and cmd == "!stop":
             self.die()
 
         # Send responses
@@ -91,18 +200,57 @@ class wormgas(SingleServerIRCBot):
             c.privmsg(nick, r)
 
     def on_pubmsg(self, c, e):
+        """This method is called when a message is sent to the channel the bot
+        is on
+
+        Arguments:
+            c: the Connection object associated with this event
+            e: the Event object"""
+
         nick = e.source().split("!")[0]
         priv = self.get_config("privlevel:%s" % nick)
         chan = e.target()
         msg = e.arguments()[0].strip()
         cmdtokens = msg.split()
+        try:
+            cmd = cmdtokens[0]
+        except IndexError:
+            cmd = None
 
         rs = []
         privrs = []
 
+        # !8ball
+
+        if cmd == "!8ball":
+            ltb = int(self.get_config("lasttime:8ball"))
+            wb = int(self.get_config("wait:8ball"))
+            if ltb < time.time() - wb:
+                rs = self.handle_8ball()
+                if "again" not in rs[0]:
+                    self.set_config("lasttime:8ball", time.time())
+            else:
+                privrs = self.handle_8ball()
+                wait = ltb + wb - int(time.time())
+                privrs.append("I am cooling down. You cannot use !8ball in %s "
+                    "for another %s seconds." % (chan, wait))
+
+        # !config
+
+        elif priv > 1 and cmd == "!config":
+            try:
+                id = cmdtokens[1]
+            except IndexError:
+                id = None
+            try:
+                value = cmdtokens[2]
+            except IndexError:
+                value = None
+            privrs = self.handle_config(id, value)
+
         # !flip
 
-        if cmdtokens[0] == "!flip":
+        elif cmd == "!flip":
             ltf = int(self.get_config("lasttime:flip"))
             wf = int(self.get_config("wait:flip"))
             if ltf < time.time() - wf:
@@ -116,7 +264,7 @@ class wormgas(SingleServerIRCBot):
 
         # !help
 
-        elif cmdtokens[0] == "!help":
+        elif cmd == "!help":
             try:
                 topic = cmdtokens[1]
             except IndexError:
@@ -125,24 +273,34 @@ class wormgas(SingleServerIRCBot):
 
         # !stop
 
-        elif (priv > 1) and (cmdtokens[0] == "!stop"):
+        elif priv > 1 and cmd == "!stop":
             self.die()
 
         # Send responses
 
         for r in rs:
-            c.privmsg(chan, r)
+            c.privmsg(chan, "%s: %s" % (nick, r))
 
         for privr in privrs:
             c.privmsg(nick, privr)
 
     def on_welcome(self, c, e):
+        """This method is called when the bot first connects to the server
+
+        Arguments:
+            c: the Connection object associated with this event
+            e: the Event object"""
+
         passwd = self.get_config("irc:nickservpass")
         c.privmsg("nickserv", "identify %s" % passwd)
         c.join(self.get_config("irc:channel"))
 
     def print_to_log(self, msg):
-        """Print to the log file, with UTC timestamp"""
+        """Print to the log file
+
+        Arguments:
+            msg: string, the message to print to the log file (timestamp and
+                 newline are not required)"""
 
         now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
         logfile = open("%s.log" % (self.abspath,), "a")
@@ -151,9 +309,13 @@ class wormgas(SingleServerIRCBot):
 
     def get_config(self, id):
         """Read a value from the configuration database
-        Returns the value, or -1 if the configuration id does not exist"""
 
-        config_value = "-1"
+        Arguments:
+            id: the config_id that you want to read
+
+        Returns: the config_value, or -1 if the config_id does not exist"""
+
+        config_value = -1
         sql = "select config_value from botconfig where config_id = ?"
         self.ccur.execute(sql, (id,))
         for r in self.ccur:
@@ -162,11 +324,17 @@ class wormgas(SingleServerIRCBot):
         return(config_value)
 
     def set_config(self, id, value):
+        """Set a configuration value in the database
+
+        Arguments:
+            id: the config_id to set
+            value: the value to set it to"""
+
         cur_config_value = self.get_config(id)
-        if value == -1:
+        if value in (-1, "-1"):
             sql = "delete from botconfig where config_id = ?"
             self.ccur.execute(sql, (id,))
-        elif cur_config_value == -1:
+        elif cur_config_value in (-1, "-1"):
             sql = ("insert into botconfig (config_id, config_value) values "
                 "(?, ?)")
             self.ccur.execute(sql, (id, value))
