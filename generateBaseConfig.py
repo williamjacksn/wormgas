87f6230907194ed1776177bc96f226fcd840621a
7heo <7heo@mail.com>
2012-10-24 11:55:23 +0200

    improved the config generator

--- generateBaseConfig.py
+++ generateBaseConfig.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python2
 import sqlite3
+from os import getenv
+
+colors = {"bash": {"red" : "\033[1;31m", "cyan" : "\033[0;36m", "green" : "\033[0;32m", "normal" : "\033[0m"}}
 
 def addIfMissing(config_id, config_value):
-	global c
+	global cursor, colors
+	shell = getenv("SHELL").split("/").pop()
 	# if c isn't defined to a sqlite connection cursor, this function will fail. It's easy to add some try / catch structure to avoid this, so feel free to do it.
-	c.execute('''SELECT * FROM botconfig WHERE config_id = "''' + config_id + '''";''')
-	if c.fetchone() == None :	
-		print('''Configuring ''' + config_id + ''' to "''' + config_value + '''"''')
-		c.execute('''INSERT INTO botconfig VALUES ("''' + config_id + '''", "''' + config_value + '''");''')
+	cursor.execute('''SELECT * FROM botconfig WHERE config_id = "''' + config_id + '''";''')
+	item = cursor.fetchone()
+	if item == None :	
+		print('''Configuring ''' + colors[shell]["cyan"] + config_id + colors[shell]["normal"] + ''' to ''' + colors[shell]["green"] + '''"''' + config_value + '''"''' + color[shell]["normal"])
+		cursor.execute('''INSERT INTO botconfig VALUES ("''' + config_id + '''", "''' + config_value + '''");''')
 	else:
-		print(config_id + ''' already configured.''')
+		print(colors[shell]["red"] + '''[WARNING]''' + colors[shell]["normal"] + ''' ''' + colors[shell]["cyan"] + config_id + colors[shell]["normal"] + ''' already configured to ''' + colors[shell]["green"] + '''"''' + item[1] + '''"''' + colors[shell]["normal"] + '''.''')
 		
 
 def main():
-	global c
+	global cursor
 	conn = sqlite3.connect('config.sqlite')
-	c = conn.cursor()
+	cursor = conn.cursor()
 	
-	addIfMissing("msg:ignore", "")
-	addIfMissing("irc:server", "irc.synirc.org")
-	addIfMissing("irc:nick",   "wormgas")
-	addIfMissing("irc:name",   "wormgas")
+	addIfMissing("msg:ignore",          "")
+	addIfMissing("irc:server",          "irc.synirc.org")
+	addIfMissing("irc:nick",            "wormgas")
+	addIfMissing("irc:name",            "wormgas")
+	addIfMissing("irc:channel",         "#rainwave")
+	addIfMissing("lasttime:forumcheck", "")
+	addIfMissing("lasttime:msg",        "")
+	addIfMissing("lasttime:musiccheck", "")
+	addIfMissing("msg:last",            "")
+	addIfMissing("timeout:chat",        "")
+	addIfMissing("timeout:forumcheck",  "")
+	addIfMissing("timeout:musiccheck",  "")
 	
 	conn.commit()
 	conn.close()
