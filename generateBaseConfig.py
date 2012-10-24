5b8569912acc9249b5102f9cabebebd501711449
7heo <7heo@mail.com>
2012-10-24 16:36:46 +0200

    Finished config generator (for now)

--- generateBaseConfig.py
+++ generateBaseConfig.py
@@ -1,21 +1,56 @@
 #!/usr/bin/env python2
 import sqlite3
 from os import getenv
+from collections import defaultdict
 
-colors = {"bash": {"red" : "\033[1;31m", "cyan" : "\033[0;36m", "green" : "\033[0;32m", "normal" : "\033[0m"}}
+colors	=	{
+				# To add a new supported shell, just add a new dict entry with its name (the name of the binary) and some values for the colors.
+				"bash":	{
+							"red"		: "\033[1;31m",
+							"cyan"		: "\033[0;36m",
+							"green"		: "\033[0;32m",
+							"normal"	: "\033[0m"
+						},
+				# default fallback. No colors to avoid compatibility problems with unknown shells. So unless the shell is explicitely supported, no colors.
+				None:	{
+							"red"		: "",
+							"cyan"		: "",
+							"green"		: "",
+							"normal"	: ""
+						},
+			}
 
 def addIfMissing(config_id, config_value):
+	# sine global variables, to store the database cursor and the shell colors.
 	global cursor, colors
+	# make sure the cursor variable is correctly set.
+	try:
+		if not isinstance(cursor, sqlite3.Cursor):
+			raise NameError
+	except NameError:
+		print("No connection to the database")
+		return False
+	# get the shell value from the system
 	shell = getenv("SHELL").split("/").pop()
-	# if c isn't defined to a sqlite connection cursor, this function will fail. It's easy to add some try / catch structure to avoid this, so feel free to do it.
+	# if the shell color aren't defined, fallback to fallback. :D
+	if not shell in colors :
+		shell = None
+	# poll for the asked config entry
 	cursor.execute('''SELECT * FROM botconfig WHERE config_id = "''' + config_id + '''";''')
+	# fetch any results in 'item'
 	item = cursor.fetchone()
-	if item == None :	
-		print('''Configuring ''' + colors[shell]["cyan"] + config_id + colors[shell]["normal"] + ''' to ''' + colors[shell]["green"] + '''"''' + config_value + '''"''' + color[shell]["normal"])
+	# if the entry does not exist
+	if item == None :
+		# add it
 		cursor.execute('''INSERT INTO botconfig VALUES ("''' + config_id + '''", "''' + config_value + '''");''')
+		# and print a message saying so
+		print('''Configuring ''' + colors[shell]["cyan"] + config_id + colors[shell]["normal"] + ''' to ''' + colors[shell]["green"] + '''"''' + config_value + '''"''' + color[shell]["normal"])
+	# else
 	else:
+		# warn the user that the entry is already configured
 		print(colors[shell]["red"] + '''[WARNING]''' + colors[shell]["normal"] + ''' ''' + colors[shell]["cyan"] + config_id + colors[shell]["normal"] + ''' already configured to ''' + colors[shell]["green"] + '''"''' + item[1] + '''"''' + colors[shell]["normal"] + '''.''')
-		
+	# and return successfuly
+	return True
 
 def main():
 	global cursor
