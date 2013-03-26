af788615b20191c91cace7f6359bfe5965a70479
William Jackson <william@subtlecoolness.com>
2013-03-26 13:27:15 -0400

    Unescape HTML entities in title tags

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
+import htmlentitydefs
 import json
 import os
+import re
 import requests
 
 class CollectionOfNamedLists:
@@ -104,6 +106,28 @@ class TitleFetcher(object):
 			raise TitleFetcherError(u'There was a problem fetching data from: {}'.format(url))
 		if u'<title>' in data.text:
 			title = data.text.partition(u'<title>')[2].partition(u'</title>')[0]
-			return u' '.join(title.split())
+			return self.unescape(u' '.join(title.split()))
 		else:
 			raise TitleFetcherError(u'There is no <title> tag at: {}'.format(url))
+
+	# from Fredrik Lundh, http://effbot.org/zone/re-sub.htm#unescape-html
+	def unescape(self, text):
+		def fixup(m):
+			text = m.group(0)
+			if text[:2] == "&#":
+				# character reference
+				try:
+					if text[:3] == "&#x":
+						return unichr(int(text[3:-1], 16))
+					else:
+						return unichr(int(text[2:-1]))
+				except ValueError:
+					pass
+			else:
+				# named entity
+				try:
+					text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+				except KeyError:
+					pass
+			return text # leave as is
+		return re.sub("&#?\w+;", fixup, text)
