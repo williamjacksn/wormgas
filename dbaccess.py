d470fabc5b9c8b2a76334893afbac21ce4899207
William Jackson <william@subtlecoolness.com>
2013-11-27 15:56:44 -0600

    Don't use generators here

--- dbaccess.py
+++ dbaccess.py
@@ -512,18 +512,20 @@ class RainwaveDatabase(object):
 		m = u'Getting favourite songs for user {} on channel {}'
 		log.info(m.format(user_id, cid))
 
+		faves = list()
 		sql = (
 			u'select song_id, album_id, song_available, song_releasetime from '
 			u'rw_songfavourites join rw_songs using (song_rating_id) where '
 			u'sid = %s and user_id = %s')
 		self.rcur.execute(sql, (cid, user_id))
 		for r in self.rcur:
-			yield {
+			faves.append({
 				u'id': int(r[0]),
 				u'album_id': int(r[1]),
 				u'available': bool(r[2]),
 				u'release_time': int(r[3])
-			}
+			})
+		return faves
 
 	def get_forum_post_info(self, post_id=None):
 		'''Return a string of information and a url for a forum post, default
@@ -760,6 +762,7 @@ class RainwaveDatabase(object):
 		m = u'Getting unrated songs for user {} on channel {}'
 		log.info(m.format(user_id, cid))
 
+		unrated = list()
 		sql = (u'with rated_songs as (select song_rating_id from '
 			u'rw_songratings where user_id = %s), unrated_songs as (select '
 			u'song_id, album_id, song_available, song_releasetime from '
@@ -773,13 +776,14 @@ class RainwaveDatabase(object):
 			u'join album_unrated_counts using (album_id)')
 		self.rcur.execute(sql, (user_id, cid))
 		for r in self.rcur:
-			yield {
+			unrated.append({
 				u'id': int(r[0]),
 				u'album_id': int(r[1]),
 				u'available': bool(r[2]),
 				u'release_time': int(r[3]),
 				u'unrated_songs_in_album': int(r[4])
-			}
+			})
+		return unrated
 
 	def request_playlist_refresh(self, cid):
 		'''Request a playlist refresh for a channel'''
