5afa54fa84fa4bcf33be67d834a978bd37fd3da1
William Jackson <william@subtlecoolness.com>
2016-01-27 11:22:04 -0600

    Don't notify if user talked in last 5 minutes

--- plugins/notify.py
+++ plugins/notify.py
@@ -1,3 +1,4 @@
+import time
 import urllib.parse
 import urllib.request
 
@@ -37,20 +38,31 @@ class NotifyHandler:
         nick = sender.lower()
         action = tokens[1]
         if action == 'show':
-            saved_key = config.get(nick)
-            if saved_key is None:
-                m = 'I do not have a secret key saved for you.'
-            else:
-                m = 'Your secret key is ' + saved_key
+            return self.handle_show(nick, bot)
         elif action == 'stop':
             config.remove(nick)
             m = ('I will no longer notify you when someone says your nick in '
                  'the channel.')
         else:
-            config[nick] = action
+            config[nick] = {'key': action, 'seen': int(time.time())}
             m = ('I will use the secret key {} to notify you when someone says '
                  'your nick in the channel.'.format(action))
         bot.send_privmsg(sender, m)
+
+    def handle_show(self, nick, bot):
+        config = self.get_config(bot)
+        try:
+            rec = config[nick]
+        except IndexError:
+            m = 'I do not have a secret key saved for you.'
+            return bot.send_privmsg(nick, m)
+        try:
+            key = rec['key']
+        except IndexError:
+            m = 'I do not have a secret key saved for you.'
+            return bot.send_privmsg(nick, m)
+        m = 'Your secret key is ' + key
+        return bot.send_privmsg(nick, m)
 
     @staticmethod
     def notify(key, sender, target, text):
@@ -61,17 +73,28 @@ class NotifyHandler:
 
     @staticmethod
     def notify_on_match(message, bot):
-        text = message.split(' :', maxsplit=1)[1]
         config = NotifyHandler.get_config(bot)
+        source = message.split()[0].lstrip(':')
+        sender, _, _ = bot.parse_hostmask(source)
+        sender_low = sender.lower()
+        if sender_low in config:
+            bot.log('** Updating last seen time for ' + sender_low)
+            rec = config[sender_low]
+            rec['seen'] = int(time.time())
+            config[sender_low] = rec
+        text = message.split(' :', maxsplit=1)[1]
         for nick in config.keys():
             if nick in text.lower():
-                source = message.split()[0].lstrip(':')
-                sender, _, _ = bot.parse_hostmask(source)
-                target = message.split()[2]
-                if bot.is_irc_channel(target):
-                    bot.log('** Sending notification to ' + nick)
-                    key = config[nick]
-                    NotifyHandler.notify(key, sender, target, text)
+                seen = int(config[nick]['seen'])
+                now = int(time.time())
+                m = '** {} last seen at {}, it is currently {}'
+                bot.log(m.format(nick, seen, now))
+                if now > seen + 300:
+                    target = message.split()[2]
+                    if bot.is_irc_channel(target):
+                        bot.log('** Sending notification to ' + nick)
+                        key = config[nick]['key']
+                        NotifyHandler.notify(key, sender, target, text)
 
     def send_help(self, target, bot):
         for line in self.help_text:
