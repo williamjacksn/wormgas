948a61953bacbd22f80a2d6201ff85eacc60da8b
William Jackson <william@subtlecoolness.com>
2017-01-12 12:29:38 -0600

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wormgas',
-    version='1.0.0',
+    version='1.0.1',
     author='William Jackson',
     author_email='william@subtlecoolness.com',
     url='https://github.com/williamjacksn/wormgas',
