1e3297216d0d59cd4e60ba28473d90a57a3d7380
William Jackson <william@subtlecoolness.com>
2017-02-02 11:27:53 -0600

    Add nick ignore list to chat plugin

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wormgas',
-    version='1.0.1',
+    version='1.0.2',
     author='William Jackson',
     author_email='william@subtlecoolness.com',
     url='https://github.com/williamjacksn/wormgas',
--- wormgas/plugins/chat.py
+++ wormgas/plugins/chat.py
@@ -10,37 +10,27 @@ class ChatHandler:
     admin = False
 
     quotes = [
-        ('Attack the evil that is within yourself, rather than attacking the '
-         'evil that is in others.'),
+        'Attack the evil that is within yourself, rather than attacking the evil that is in others.',
         'Before you embark on a journey of revenge, dig two graves.',
         'Better a diamond with a flaw than a pebble without.',
         'Everything has beauty, but not everyone sees it.',
         'He who knows all the answers has not been asked all the questions.',
-        ('He who learns but does not think, is lost! He who thinks but does '
-         'not learn is in great danger.'),
+        'He who learns but does not think, is lost! He who thinks but does not learn is in great danger.',
         'I hear and I forget. I see and I remember. I do and I understand.',
-        ('If what one has to say is not better than silence, then one should '
-         'keep silent.'),
-        ('If you make a mistake and do not correct it, this is called a '
-         'mistake.'),
+        'If what one has to say is not better than silence, then one should keep silent.',
+        'If you make a mistake and do not correct it, this is called a mistake.',
         'Ignorance is the night of the mind but a night without moon and star.',
-        ('Music produces a kind of pleasure which human nature cannot do '
-         'without.'),
+        'Music produces a kind of pleasure which human nature cannot do without.',
         'Only the wisest and stupidest of men never change.',
-        ('Our greatest glory is not in never falling, but in rising every time '
-         'we fall.'),
+        'Our greatest glory is not in never falling, but in rising every time we fall.',
         'Respect yourself and others will respect you.',
         'Silence is a true friend who never betrays.',
-        ('The hardest thing of all is to find a black cat in a dark room, '
-         'especially if there is no cat.'),
-        ('The man who asks a question is a fool for a minute, the man who does '
-         'not ask is a fool for life.'),
+        'The hardest thing of all is to find a black cat in a dark room, especially if there is no cat.',
+        'The man who asks a question is a fool for a minute, the man who does not ask is a fool for life.',
         'The superior man is modest in his speech, but exceeds in his actions.',
         'To be wronged is nothing, unless you continue to remember it.',
-        ('To see what is right and not to do it, is want of courage or of '
-         'principle.'),
-        ('What you know, you know, what you do not know, you do not know. This '
-         'is true wisdom.')
+        'To see what is right and not to do it, is want of courage or of principle.',
+        'What you know, you know, what you do not know, you do not know. This is true wisdom.'
     ]
 
     def __init__(self, sbot):
@@ -55,8 +45,7 @@ class ChatHandler:
             if last < now - wait:
                 if not bot.in_channel:
                     if bot.debug:
-                        m = '** Would revive chat, but not currently in channel'
-                        bot.log(m)
+                        bot.log('** Would revive chat, but not currently in channel')
                     return
                 if bot.debug:
                     bot.log('** Reviving chat')
@@ -69,21 +58,22 @@ class ChatHandler:
             else:
                 remaining = last + wait - now
                 if bot.debug:
-                    m = '** Will revive chat in {} seconds'.format(remaining)
-                    bot.log(m)
+                    bot.log('** Will revive chat in {} seconds'.format(remaining))
 
         @sbot.ee.on('PRIVMSG')
         def handle_privmsg(message, bot):
             tokens = message.split()
             source = tokens[0].lstrip(':')
             source_nick, _, _ = bot.parse_hostmask(source)
+            if source_nick.lower() in bot.c.get('chat:ignore_nicks', '').split():
+                bot.log('** {} is in the chat:ignore_nicks list')
+                return
             target = tokens[2]
             cmd = tokens[3].lstrip(':').lower()
             builtin_commands = {'!load', '!help'}
             public_commands = set(bot.plug_commands.keys())
             admin_commands = set(bot.plug_commands_admin.keys())
-            all_commands = builtin_commands | public_commands | admin_commands
-            if cmd in all_commands:
+            if cmd in builtin_commands | public_commands | admin_commands:
                 return
 
             text = message.split(' :', maxsplit=1)[1]
@@ -110,8 +100,7 @@ class ChatHandler:
             else:
                 bot.send_privmsg(source_nick, response)
                 remaining = last + wait - now
-                m = 'I am cooling down. I cannot respond in {}'.format(target)
-                m = '{} for another {} seconds.'.format(m, remaining)
+                m = 'I am cooling down. I cannot respond in {} for another {} seconds.'.format(target, remaining)
                 bot.send_privmsg(source_nick, m)
 
     def reply(self, text, bot, learn=True):
