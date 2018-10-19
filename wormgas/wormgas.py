d0fbcea93422878361d4ad0f9a5b06b7fb18e8ab
William Jackson <william@subtlecoolness.com>
2018-10-19 13:47:22 -0500

    Read version info from Dockerfile

--- wormgas/wormgas.py
+++ wormgas/wormgas.py
@@ -16,10 +16,23 @@ class Wormgas(cmds.Bot):
         self.session = aiohttp.ClientSession(loop=self.loop)
 
 
+def version():
+    """Read version from Dockerfile"""
+    dockerfile = pathlib.Path(__file__).resolve().parent.parent / 'Dockerfile'
+    with open(dockerfile) as f:
+        for line in f:
+            if 'org.label-schema.version' in line:
+                return line.strip().split('=', maxsplit=1)[1]
+    return 'unknown'
+
+
 def main():
     log_format = os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
     log_level = os.getenv('LOG_LEVEL', 'INFO')
-    logging.basicConfig(level=log_level, format=log_format, stream=sys.stdout)
+    logging.basicConfig(level='DEBUG', format=log_format, stream=sys.stdout)
+    logging.debug(f'wormgas {version()}')
+    logging.debug(f'Changing log level to {log_level}')
+    logging.getLogger().setLevel(log_level)
     config_file = os.getenv('CONFIG_FILE', '/opt/wormgas/_config.json')
     bot = Wormgas(config_path=pathlib.Path(config_file).resolve(), command_prefix='!', pm_help=True)
     bot.load_extension('wormgas.cogs.chat')
