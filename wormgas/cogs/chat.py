8d1ed048a667e28743883711827bab537ab24386
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-12-31 09:42:53 -0600

    Fix decorator

--- wormgas/cogs/chat.py
+++ wormgas/cogs/chat.py
@@ -41,7 +41,7 @@ class ChatCog(discord.ext.commands.Cog):
         brain_file = pathlib.Path(os.getenv('BRAIN_FILE', '/etc/wormgas/_brain.sqlite'))
         self.brain = wormgas.cogs.cobe.brain.Brain(str(brain_file))
 
-    @discord.ext.commands.command
+    @discord.ext.commands.command()
     async def mention(self, ctx: discord.ext.commands.Context, watch_word: str):
         normalized_watch_word = watch_word.lower()
         await ctx.author.send(f'Okay, I will ping you whenever I see a message that contains {normalized_watch_word!r}')
