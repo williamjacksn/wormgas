c99fb5753cd66be72f556cb8f605f2f614756756
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-07-01 12:09:45 -0500

    Print logs after restarting service

--- ci/ssh-deploy.sh
+++ ci/ssh-deploy.sh
@@ -3,3 +3,4 @@ echo "${SSH_PRIVATE_KEY}" > ~/.ssh/id_ed25519
 ssh-keyscan -H -p 2234 "${SSH_HOST}" > ~/.ssh/known_hosts
 ssh ssh://${SSH_USER}@${SSH_HOST}:2234 git -C /home/rainwave/wormgas pull --ff-only
 ssh ssh://${SSH_USER}@${SSH_HOST}:2234 sudo systemctl restart wormgas.service
+ssh ssh://${SSH_USER}@${SSH_HOST}:2234 journalctl --invocation=0 --output=short-iso --unit=wormgas.service
